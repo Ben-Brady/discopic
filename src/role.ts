2e01b35dc4e55887a088cb75d9a30714949eab13
Ben Brady <benbrady04.bb@gmail.com>
2024-09-11 18:32:05 +0100

    docs(role): added docstring

--- src/role.ts
+++ src/role.ts
@@ -1,12 +1,13 @@
 import { Guild, Role, type RoleCreateOptions } from "discord.js";
 
-export const upsertRole = async (
-    guild: Guild,
-    config: RoleCreateOptions,
-): Promise<Role> => {
-    const existingRole = guild.roles.cache.find(
-        role => role.name === config.name,
-    );
+/**
+ * Create a role, if it already exists return it
+ * @param {Guild} guild
+ * @param {Config} config
+ * @returns {Promise<Role>}
+ */
+export const upsertRole = async (guild: Guild, config: RoleCreateOptions): Promise<Role> => {
+    const existingRole = guild.roles.cache.find(role => role.name === config.name);
 
     if (existingRole) {
         return existingRole;
